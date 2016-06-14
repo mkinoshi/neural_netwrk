b5c91f3125708424b37a7b0eb10f93433d1673cb
Makoto <mak.1213.11@gmail.com>
2016-06-13 21:32:52 -0400

    One hidden Layer

--- neural_net.py
+++ neural_net.py
@@ -6,14 +6,7 @@ def sig(sig_input, der=False):
 	else:
 		return 1/(1+np.exp(-sig_input))
 
-
-def main():
-	#creating inputs values
-	X = np.array([[0,0,1],
-				  [1,1,1],
-				  [1,0,1],
-				  [0,1,1]])
-	y = np.array([[0],[1],[1],[0]])
+def single(inputs, output):
 	learning_r = 0.1
 	#assigining the weights randomly
 	#first create the seed
@@ -23,11 +16,11 @@ def main():
 
 	for i in range(1000):
 		#forward propagation
-		i_0 = X
+		i_0 = inputs
 		i_1 = sig(np.dot(i_0,weights))
 
 		#calculate the error
-		error = y - i_1
+		error = output - i_1
 
 		#calculate the delta value
 		delta = learning_r * np.dot(i_0.T, (error * sig(i_1, True)))
@@ -37,7 +30,49 @@ def main():
 		if i % 100 == 0:
 			print "complete", (10*i)/100, " %"
 			
-	print i_1
+	return i_1
+
+def oneHidden(inputs, output):
+	learning_r = 1
+
+	#initializing the weights; weight_1 and weight_2
+	np.random.seed(1)
+	weight_1 = 2*np.random.rand(3,4) - 1
+	weight_2 = 2*np.random.rand(4,1) - 1
+
+	for i in range(60000):
+		#forward propagation
+		i_0 = inputs
+		i_1 = sig(np.dot(i_0,weight_1))
+		i_2 = sig(np.dot(i_1,weight_2))
+
+		#calculate the error at layer i_2
+		i_2_error = output - i_2
+		if i % 1000 == 0:
+			print "complete", (10*i)/100, " %, Error:" + str(np.mean(np.abs(i_2_error)))
+		#calculate the delta at i_2 
+		i_2_delta = i_2_error*sig(i_2, True)
+
+		#calculat how much each l_1 contribute to the i_2 values
+		i_1_error = i_2_delta.dot(weight_2.T)
+
+		#calculate the delta at i_1
+		i_1_delta = i_1_error*sig(i_1, True)
+
+		weight_2 += learning_r*np.dot(i_1.T, i_2_delta)
+		weight_1 += learning_r*np.dot(i_0.T, i_1_delta)
+	
+	return i_2
+def main():
+	#creating inputs values
+	X = np.array([[0,0,1],
+				  [0,1,1],
+				  [1,0,1],
+				  [1,1,1]])
+	y = np.array([[0],[1],[1],[0]])
+	#res = single(X,y)
+	res = oneHidden(X,y)
+	print res
 
 if __name__ == "__main__":
 	main()
